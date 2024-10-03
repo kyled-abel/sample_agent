f12a35167462c2f3fdc0d735bf45e70c61ade8cc
Kaile Ding <kyledingkd@gmail.com>
2024-10-03 00:30:37 -0700

    Master agent to break down the operations.

--- master_agent/master_agent.py
+++ master_agent/master_agent.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from langchain_openai import ChatOpenAI
+from langchain.agents import AgentExecutor
+from langchain.agents.format_scratchpad import format_to_openai_function_messages
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.prompts.prompt import PromptTemplate
 import os
 import openai
 import requests
@@ -10,10 +15,55 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 @app.route('/')
 def hello_world():
     llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-    output = llm.invoke("What would be the AI equivalent of Hello World?")
-    
-    response = requests.get('http://agent1:5001/')
-    return f"Output from master agent: {output.content}, Output from agent1: {response.text}"
+    tools=[]
+
+    examples = [
+        {
+            "question": "1+1=?",
+            "answer": "Step1. (1+1)"
+        },
+        {
+            "question": "1+1*2=?",
+            "answer": "Step1. (1*2)\nStep2. (1+answer)"
+        },
+        {
+            "question": "(1+1)*2=?",
+            "answer": "Step1. (1+1)\nStep2. (answer*2)"
+        },
+        {
+            "question": "(1+1)*2*2+1=?",
+            "answer": "Step1. (1+1)\nStep2. (answer*2)\nStep3. (answer*2)\nStep4. (answer+1)"
+        },
+        {
+            "question": "1+1*2*2+1=?",
+            "answer": "Step1. (1*2)\nStep2. (answer*2)\nStep3. (1+answer)\nStep4. (answer+1)"
+        },
+    ]
+
+    example_prompt = PromptTemplate(
+        input_variables=["question", "answer"],
+        template="Question: {question}\n{answer}",
+    )
+    prompt = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt,
+        suffix="Question: {input}",
+        input_variables=["input"],
+    )
+
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_to_openai_function_messages(x["intermediate_steps"]),
+        }
+        | prompt
+        | llm
+        | OpenAIFunctionsAgentOutputParser()
+    )
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    output = agent_executor.invoke({"input": "1+1+1*2*3+1=?"})
+    return f"{output['output']}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

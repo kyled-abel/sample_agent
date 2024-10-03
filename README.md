6733654ad568f566ae66e0f3b8ac0186f9d98a05
Kaile Ding <kyledingkd@gmail.com>
2024-10-02 21:52:54 -0700

    Create Flask app to call the agents

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sample_agent
 
-To build the Docker container, run `docker build -t master_agent master_agent`
+To build the Docker containers, run `docker-compose build`
 
 To run the Docker containter, run `docker run -e OPENAI_API_KEY=$YourApiKeyHere$ master_agent`
--- agent1/agent1.py
+++ agent1/agent1.py
@@ -1,10 +1,17 @@
+from flask import Flask
 from langchain_openai import ChatOpenAI
 import os
 import openai
- 
+
+app = Flask(__name__)
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+@app.route('/')
+def hello_world():
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-output = llm.invoke("What would be the AI equivalent of Hello World?")
-print(output)
+    output = llm.invoke("What would be the AI equivalent of Hello World?")
+    return output
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
